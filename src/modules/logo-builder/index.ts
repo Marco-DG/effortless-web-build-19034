37cc92ce23e5f496ae5ea2da21306aebc73840bb
Marco D.G. <dg.account@protonmail.com>
2025-11-14 15:09:14 +0100

    ongoing refactor

--- src/modules/logo-builder/index.ts
+++ src/modules/logo-builder/index.ts
@@ -1,11 +1,8 @@
 export { LogoBuilderRedesigned } from './LogoBuilderRedesigned';
 export { InteractiveLogoCanvas } from './InteractiveLogoCanvas';
 export { LogoPreview } from './LogoPreview';
-export { LogoControls } from './LogoControls';
 
 // Sections
 export { TemplateSection } from './sections/TemplateSection';
-export { FontSection } from './sections/FontSection';
-export { StyleSection } from './sections/StyleSection';
 export { CanvasSection } from './sections/CanvasSection';
 export { LayersSection } from './sections/LayersSection';
