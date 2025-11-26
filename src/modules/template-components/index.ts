d61e612c3113163e670bac289509671e81e06e06
Marco D.G. <dg.account@protonmail.com>
2025-11-26 14:45:53 +0100

    NUKE LEGACY

--- src/modules/template-components/index.ts
+++
@@ -1,7 +0,0 @@
-// Export principale per il sistema modulare
-export { ModularSiteBuilder } from './ModularSiteBuilder';
-export { ComponentBuilder } from './ComponentBuilder';
-export { ComponentEditor } from './ComponentEditor';
-export { COMPONENT_REGISTRY } from './component-registry';
-export * from './types';
-export * from './data-adapter';
