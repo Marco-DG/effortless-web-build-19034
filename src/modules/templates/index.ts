31f9dad23fdd2b26a6f1a664cf56c2626480581a
Marco D.G. <dg.account@protonmail.com>
2025-11-26 14:50:42 +0100

    NUKE LEGACY

--- src/modules/templates/index.ts
+++ src/modules/templates/index.ts
@@ -1,6 +1,3 @@
 // Export per il sistema template
 export { TemplateSelector } from './TemplateSelector';
-export { TemplateCustomizer } from './TemplateCustomizer';
-export { TemplateManager } from './TemplateManager';
-export { TEMPLATES } from './templates-data';
 export * from './types';
